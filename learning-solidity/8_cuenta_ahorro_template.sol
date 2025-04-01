6b743f73777deb50937eab9d5425e9e414e97b8d
liz-durang <lizeth.durang@gmail.com>
2025-04-01 10:21:20 -0600

    8_template added

---
+++ learning-solidity/8_cuenta_ahorro_template.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Escribir un smart contract  que gestione una cuenta de ahorro
+// 2. Hacer pruebas unitarias en .test o en Remix EVM 
+// 3. Hacer deploy en testnet 
+// 4. Realizar mínimo 4 transacciones en el smart contract
+// 5. Compartir address del contrato 
+
+
+pragma solidity ^0.8.0;
+
+contract CuentaAhorro {
+    
+    // Structura de Persona
+
+
+    // Mapeo que relaciona la dirección con la persona
+
+
+    // Función. Crear una nueva persona (solo se requiere nombre y edad)
+
+        // Verificar si ya existe una persona con esta dirección
+
+        // S no existe, Crear la nueva persona con saldo inicial de 0
+
+    // Función. ingresar saldo
+
+        // Verificar que el depósito sea positivo
+
+
+        // Actualizar saldo en la persona
+
+
+    // Función. Retirar saldo
+
+        // Verificar que el saldo sea suficiente
+
+        // Restar el saldo directamente
+
+    // Función. Consultar el saldo
+
+}
